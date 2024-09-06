f42b300b3f89217525bd4b5e1a2ef159bfea2d55
permissionBRICK <40219477+permissionBRICK@users.noreply.github.com>
2024-09-06 16:49:01 +0200

    update for hass.helpers deprecation

--- lockuser/__init__.py
+++ lockuser/__init__.py
@@ -1,10 +1,10 @@
 """Example Load Platform integration."""
 DOMAIN = 'lockuser'
-
+from homeassistant.helpers.discovery import load_platform
 
 def setup(hass, config):
     """Your controller/hub specific code."""
 
-    hass.helpers.discovery.load_platform('switch', DOMAIN, {}, config)
+    load_platform(hass, 'switch', DOMAIN, {}, config)
 
     return True
